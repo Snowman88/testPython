87a49bd2a45a242ad43e4600963a57a650e6277b
Snowman88 <taa_bo_taa_bo@TatsuyanoiMac.local>
2013-10-10 22:01:50 +0900

    test

---
+++ makedir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding:utf-8
+import os
+# for i in range(0,14):
+#     name = "20{0:02d}".format(i)
+#     os.mkdir(name)
+
+# for i in range(1, 12):
+#     name = "test/2013/{0:02d}".format(i)
+#     os.mkdir(name)
+
+
+# for i in range(1, 11):
+#     name = "test/2013/10/{0:02d}".format(i)
+#     os.mkdir(name)
